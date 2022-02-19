d521e8416ff3d02162fa634fbde081cd0af3f37f
jorgepardor <jorgeluis.mail@gmail.com>
2022-02-19 20:06:44 +0000

    lógica del semáforo de coches, con diferentes puntos de inicio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,12 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import TrafficLight from "./trafficlight.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="App">
+			<TrafficLight initialValue={0} />
+			<TrafficLight initialValue={1} />
 		</div>
 	);
 };
